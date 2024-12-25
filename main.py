a968717551c381d96d96d099d2b388464c069da8
root <root@GreenCloud.1712896505>
2024-12-25 10:22:48 -0500

    update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import re
 import telebot
 from telebot.async_telebot import AsyncTeleBot
 from telebot.types import  Message
+from md2tgmd import escape
 
 gemini_player_dict = {}
 gemini_pro_player_dict = {}
@@ -21,7 +22,7 @@ generation_config = {
     "temperature": 1,
     "top_p": 1,
     "top_k": 1,
-    "max_output_tokens": 2048,
+    "max_output_tokens": 1024,
 }
 
 safety_settings = [
@@ -41,97 +42,6 @@ safety_settings = [
         "threshold": "BLOCK_NONE"
     },
 ]
-
-def find_all_index(str, pattern):
-    index_list = [0]
-    for match in re.finditer(pattern, str, re.MULTILINE):
-        if match.group(1) != None:
-            start = match.start(1)
-            end = match.end(1)
-            index_list += [start, end]
-    index_list.append(len(str))
-    return index_list
-
-def replace_all(text, pattern, function):
-    poslist = [0]
-    strlist = []
-    originstr = []
-    poslist = find_all_index(text, pattern)
-    for i in range(1, len(poslist[:-1]), 2):
-        start, end = poslist[i : i + 2]
-        strlist.append(function(text[start:end]))
-    for i in range(0, len(poslist), 2):
-        j, k = poslist[i : i + 2]
-        originstr.append(text[j:k])
-    if len(strlist) < len(originstr):
-        strlist.append("")
-    else:
-        originstr.append("")
-    new_list = [item for pair in zip(originstr, strlist) for item in pair]
-    return "".join(new_list)
-
-def escapeshape(text):
-    return "▎*" + text.split()[1] + "*"
-
-def escapeminus(text):
-    return "\\" + text
-
-def escapebackquote(text):
-    return r"\`\`"
-
-def escapeplus(text):
-    return "\\" + text
-
-def escape(text, flag=0):
-    # In all other places characters
-    # _ * [ ] ( ) ~ ` > # + - = | { } . !
-    # must be escaped with the preceding character '\'.
-    text = re.sub(r"\\\[", "@->@", text)
-    text = re.sub(r"\\\]", "@<-@", text)
-    text = re.sub(r"\\\(", "@-->@", text)
-    text = re.sub(r"\\\)", "@<--@", text)
-    if flag:
-        text = re.sub(r"\\\\", "@@@", text)
-    text = re.sub(r"\\", r"\\\\", text)
-    if flag:
-        text = re.sub(r"\@{3}", r"\\\\", text)
-    text = re.sub(r"_", "\_", text)
-    text = re.sub(r"\*{2}(.*?)\*{2}", "@@@\\1@@@", text)
-    text = re.sub(r"\n{1,2}\*\s", "\n\n• ", text)
-    text = re.sub(r"\*", "\*", text)
-    text = re.sub(r"\@{3}(.*?)\@{3}", "*\\1*", text)
-    text = re.sub(r"\!?\[(.*?)\]\((.*?)\)", "@@@\\1@@@^^^\\2^^^", text)
-    text = re.sub(r"\[", "\[", text)
-    text = re.sub(r"\]", "\]", text)
-    text = re.sub(r"\(", "\(", text)
-    text = re.sub(r"\)", "\)", text)
-    text = re.sub(r"\@\-\>\@", "\[", text)
-    text = re.sub(r"\@\<\-\@", "\]", text)
-    text = re.sub(r"\@\-\-\>\@", "\(", text)
-    text = re.sub(r"\@\<\-\-\@", "\)", text)
-    text = re.sub(r"\@{3}(.*?)\@{3}\^{3}(.*?)\^{3}", "[\\1](\\2)", text)
-    text = re.sub(r"~", "\~", text)
-    text = re.sub(r">", "\>", text)
-    text = replace_all(text, r"(^#+\s.+?$)|```[\D\d\s]+?```", escapeshape)
-    text = re.sub(r"#", "\#", text)
-    text = replace_all(
-        text, r"(\+)|\n[\s]*-\s|```[\D\d\s]+?```|`[\D\d\s]*?`", escapeplus
-    )
-    text = re.sub(r"\n{1,2}(\s*)-\s", "\n\n\\1• ", text)
-    text = re.sub(r"\n{1,2}(\s*\d{1,2}\.\s)", "\n\n\\1", text)
-    text = replace_all(
-        text, r"(-)|\n[\s]*-\s|```[\D\d\s]+?```|`[\D\d\s]*?`", escapeminus
-    )
-    text = re.sub(r"```([\D\d\s]+?)```", "@@@\\1@@@", text)
-    text = replace_all(text, r"(``)", escapebackquote)
-    text = re.sub(r"\@{3}([\D\d\s]+?)\@{3}", "```\\1```", text)
-    text = re.sub(r"=", "\=", text)
-    text = re.sub(r"\|", "\|", text)
-    text = re.sub(r"{", "\{", text)
-    text = re.sub(r"}", "\}", text)
-    text = re.sub(r"\.", "\.", text)
-    text = re.sub(r"!", "\!", text)
-    return text
 
 # Prevent "create_convo" function from blocking the event loop.
 async def make_new_gemini_convo():
