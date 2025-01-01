67fde6a15e0024b79bd4259a7cb87b7ba4061558
H-T-H <serendipity.hth@gmail.com>
2025-01-01 04:37:13 -0500

    bug fix

--- handers.py
+++ handers.py
@@ -93,7 +93,7 @@ async def gemini_photo_handler(message: Message, bot: TeleBot) -> None:
         }
         try:
             await bot.edit_message_text(before_generate_info, chat_id=sent_message.chat.id, message_id=sent_message.message_id)
-            response = await async_generate_content(model, contents)
+            response = await gemini.async_generate_content(model, contents)
             await bot.edit_message_text(response.text, chat_id=sent_message.chat.id, message_id=sent_message.message_id)
         except Exception:
             traceback.print_exc()
@@ -114,7 +114,7 @@ async def gemini_photo_handler(message: Message, bot: TeleBot) -> None:
         }
         try:
             await bot.edit_message_text(before_generate_info, chat_id=sent_message.chat.id, message_id=sent_message.message_id)
-            response = await async_generate_content(model, contents)
+            response = await gemini.async_generate_content(model, contents)
             await bot.edit_message_text(response.text, chat_id=sent_message.chat.id, message_id=sent_message.message_id)
         except Exception:
             traceback.print_exc()
