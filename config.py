e73f01f7f42d867b3474ce997a57b408353076b9
H-T-H <serendipity.hth@gmail.com>
2025-02-10 10:19:16 -0500

    update

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ conf = {
     "error_info":           "⚠️⚠️⚠️\nSomething went wrong !\nplease try to change your prompt or contact the admin !",
     "before_generate_info": "🤖Generating🤖",
     "download_pic_notify":  "🤖Loading picture🤖",
-    "model_1":              "gemini-2.0-flash-exp",
+    "model_1":              "gemini-2.0-flash",
     "model_2":              "gemini-1.5-pro-latest",
     "n": 30  #Number of historical records to keep
 }
