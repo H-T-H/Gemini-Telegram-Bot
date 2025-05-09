5b0a21fc5be41e32de1ca0bb50529a687ded538f
老果冻 <2016101158@nit.edu.cn>
2025-05-09 11:05:56 +0800

    model_3 模型更改

--- config.py
+++ config.py
@@ -86,7 +86,7 @@ command_descriptions = {
 conf = {
     "model_1": "gemini-2.5-flash-preview-04-17",
     "model_2": "gemini-2.5-pro-exp-03-25",
-    "model_3": "gemini-2.0-flash-exp", #for draw
+    "model_3": "gemini-2.0-flash-preview-image-generation", #for draw, updated model name
     "streaming_update_interval": 0.5,  # Streaming answer update interval (seconds)
     "default_language": "zh"  # 默认语言
 }
